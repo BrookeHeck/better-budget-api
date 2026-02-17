2925e2145dbbc68a6c8ba9f42c0a307eeafada63
BrookeHeck <b19heck@gmail.com>
2026-02-17 18:07:18 -0500

    create account open api paths and component files and add account controller

---
+++ src/main/java/com/betterbudget/budget/api/controller/AccountController.java
@@ -0,0 +1,37 @@
+package com.betterbudget.budget.api.controller;
+
+import org.jspecify.annotations.Nullable;
+import org.openapitools.api.AccountApi;
+import org.openapitools.model.Account;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController implements AccountApi {
+    @Override
+    public ResponseEntity<Account> createAccount(@Nullable Account account) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteAccount(Long accountId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Account> getAccount(Long accountId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Account>> getUserAccounts(Long userId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Account> updateAccount(@Nullable Account account) {
+        return null;
+    }
+}
