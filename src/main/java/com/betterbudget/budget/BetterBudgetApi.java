383fa984e986446ef0a0e31bfe31ea0e2e52f7b3
Brooke Heck <brookeheck@Brookes-MacBook-Air.local>
2026-03-29 16:08:17 -0500

    update name

---
+++ src/main/java/com/betterbudget/budget/BetterBudgetApi.java
@@ -0,0 +1,13 @@
+package com.betterbudget.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterBudgetApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterBudgetApi.class, args);
+	}
+
+}
