71314752adb069d97b1b4c1f461f6448db0b4839
BrookeHeck <b19heck@gmail.com>
2026-03-29 20:42:56 -0500

    implement recurring payment api methods

--- src/main/java/com/betterbudget/budget/api/controller/RecurringPaymentController.java
+++ src/main/java/com/betterbudget/budget/api/controller/RecurringPaymentController.java
@@ -1,8 +1,11 @@
 package com.betterbudget.budget.api.controller;
 
+import com.betterbudget.budget.api.advice.BetterBudgetApiException;
+import com.betterbudget.budget.service.RecurringPaymentService;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.api.RecurringPaymentsApi;
 import org.openapitools.model.RecurringPayment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,27 +20,35 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class RecurringPaymentController implements RecurringPaymentsApi {
+    private final RecurringPaymentService recurringPaymentService;
+
     @Override
     @PostMapping("/recurring-payment")
     public ResponseEntity<RecurringPayment> createRecurringPayment(RecurringPayment recurringPayment) {
-        return null;
+        if(recurringPayment.getRecurringPaymentId() != null) {
+            throw new BetterBudgetApiException("Recurring payment id must be null on creation.", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(recurringPaymentService.saveRecurringPayment(recurringPayment), HttpStatus.CREATED);
     }
 
     @Override
     @DeleteMapping("/recurring-payment/{recurringPaymentId}")
     public ResponseEntity<Void> deleteRecurringPayment(Long recurringPaymentId) {
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     @GetMapping("/recurring-payment/{userId}")
     public ResponseEntity<List<RecurringPayment>> getRecurringPaymentsByUserId(Long userId) {
-        return null;
+        return new ResponseEntity<>(recurringPaymentService.getRecurringPaymentsByUserId(userId), HttpStatus.OK);
     }
 
     @Override
     @PutMapping("/recurring-payment")
     public ResponseEntity<RecurringPayment> updateRecurringPayment(RecurringPayment recurringPayment) {
-        return null;
+        if(recurringPayment.getRecurringPaymentId() == null) {
+            throw new BetterBudgetApiException("Recurring payment id must not be null on update.", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(recurringPaymentService.saveRecurringPayment(recurringPayment), HttpStatus.OK);
     }
 }
